50531c80e2cfdbff9f9aeee7e7579779ab7f6c42
Klau-Sam <klaudynasamelak@gmail.com>
2024-01-11 19:43:45 +0100

    Add input-atom

---
+++ libs/common-ui/src/lib/atoms/Input/Input.stories.tsx
@@ -0,0 +1,47 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Input } from './Input';
+
+const meta = {
+  title: 'UI/Atoms/Input',
+  component: Input,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Input>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    type: 'text',
+    id: 'txt-input',
+    label: 'Write something:',
+  },
+};
+
+export const Primary: Story = {
+  args: {
+    type: 'text',
+    id: 'fname',
+    label: 'First name:',
+    isRequired: true,
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    type: 'email',
+    id: 'email-input',
+    label: 'Type an e-mail:',
+    value: 'e.g. xyz@gmail.com',
+  },
+};
+
+export const Tertiary: Story = {
+  args: {
+    type: 'checkbox',
+    id: 'checkbox-input',
+    label: 'Check if you agree: ',
+  },
+};
