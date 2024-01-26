3af6f23ee23356fa08117712d0fc31ac1ce7614c
Klau-Sam <klaudynasamelak@gmail.com>
2024-01-26 23:54:24 +0100

    RegistrationForm: Add age reference

--- apps/frontend/src/app/components/RegistrationForm/RegistrationForm.tsx
+++ apps/frontend/src/app/components/RegistrationForm/RegistrationForm.tsx
@@ -4,33 +4,26 @@ import { Button, Input } from '@ems/common-ui';
 import './RegistrationForm.module.css';
 
 export const RegistrationForm = () => {
-  const [age, setAge] = useState<number>(10);
   const firstNameRef = useRef<HTMLInputElement>(null);
   const lastNameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
 
   const handleSubmit: FormEventHandler = (event) => {
     event.preventDefault();
   };
 
   return (
-    <div>
-      <div>Current age: {age}</div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <Input label="First name: " ref={firstNameRef} />
-        </div>
-        <div>
-          <Input label="Last name: " ref={lastNameRef} />
-        </div>
-        <div>
-          <Input
-            type="number"
-            label="Age: "
-            onChange={(e) => setAge(parseInt(e.target.value, 10))}
-          />
-        </div>
-        <Button type="submit" label="Send" />
-      </form>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <Input label="First name: " ref={firstNameRef} />
+      </div>
+      <div>
+        <Input label="Last name: " ref={lastNameRef} />
+      </div>
+      <div>
+        <Input type="number" label="Age: " ref={ageRef} />
+      </div>
+      <Button type="submit" label="Send" />
+    </form>
   );
 };
