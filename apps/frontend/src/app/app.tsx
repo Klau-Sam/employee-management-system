311c38a75a830050434601e78a27d8ec3f7d32f9
Klau-Sam <klaudynasamelak@gmail.com>
2024-01-29 23:54:25 +0100

    Creating pages & routes

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,9 +1,40 @@
-import { Input } from '@ems/common-ui';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Layout } from './components/Layout';
+import { HomePage } from './pages/HomePage';
+import { AboutPage } from './pages/AboutPage';
+import { ROUTE } from './routes';
+import { ContactPage } from './pages/ContactPage';
+import { EmployeesPage } from './pages/EmployeesPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: ROUTE.HOME,
+        element: <HomePage />,
+      },
+      {
+        path: ROUTE.ABOUT,
+        element: <AboutPage />,
+      },
+      {
+        path: ROUTE.CONTACT,
+        element: <ContactPage />,
+      },
+      {
+        path: ROUTE.EMPLOYEES,
+        element: <EmployeesPage />,
+      },
+    ],
+  },
+]);
 
 export function App() {
   return (
     <div>
-      <Input type="text" id="welcome-input" label="Write welcome message: " />
+      <RouterProvider router={router} />
     </div>
   );
 }
