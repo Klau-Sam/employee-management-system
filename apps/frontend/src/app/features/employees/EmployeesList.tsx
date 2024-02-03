957ec949d20528c4638717ca0290e1873b9e7340
Klau-Sam <klaudynasamelak@gmail.com>
2024-02-03 23:59:26 +0100

    Addubg EmployeesList component

---
+++ apps/frontend/src/app/features/employees/EmployeesList.tsx
@@ -0,0 +1,41 @@
+import { Header } from '@ems/common-ui';
+import { useEffect, useState } from 'react';
+
+interface UserDto {
+  name: {
+    first: string;
+    last: string;
+  };
+  email: string;
+  id: {
+    name: string;
+  };
+}
+
+export const EmployeesList = () => {
+  const [data, setData] = useState<UserDto[]>([]);
+
+  useEffect(() => {
+    fetch('https://randomuser.me/api/?results=5')
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        //
+      })
+      .then((responseData) => setData(responseData.results));
+  }, []);
+
+  return (
+    <div>
+      <Header>List</Header>
+      {data.map((elem) => (
+        <div key={elem.id.name}>
+          <div>
+            {elem.name.first} {elem.name.last}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
